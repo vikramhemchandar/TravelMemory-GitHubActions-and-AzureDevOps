049ca83366eb46bee8efdc59371cf9e6e0904595
Vikram hem Chandar <vikramhemchandar@gmail.com>
2026-01-24 00:59:22 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any // Tells Jenkins where to run the pipeline
+    stages {
+        stage('checkout') {
+            steps {
+                echo 'Checking out the code from repository...'
+                git 'https://github.com/vikramhemchandar/TravelMemory.git'
+            }
+        }
+
+        stage('install') {
+            steps {
+                echo 'Installing the application...'
+                sh 'cd backend'
+                sh 'npm install'
+            }
+        }
+
+        stage('build') { 
+            steps {
+                echo 'Building the application...'
+                sh 'cd backend'
+                sh 'npm run build'
+            }
+        }
+    }
+}
